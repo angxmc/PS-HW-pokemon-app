18f6e43d8f95a963d02cdc02e1df98f82777b085
angxmc <2017xchen@gmail.com>
2023-09-07 00:20:29 -0400

    index.jsx rendered at pokemon route

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 //require express
 const express = require("express");
 //require the database of pokemon
-const pokemon = require('./models/pokemon')
-
+const pokemon = require("./models/pokemon");
+//require jsx-engine
+const jsxEngine = require('jsx-view-engine')
 //set express to a variable
 const app = express();
 //set a variable of port to 3000
 const port = 3000;
 
+app.set("view engine", "jsx");
+app.engine("jsx", jsxEngine());
+
+
 //set app to listen to the port and console log it so we know it's working
-app.listen(port, () =>{
-    console.log(`app is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`app is running on port: ${port}`);
+});
 
 //include an route / that will display 'Welcome to the Pokemon App!
-app.get('/', (req, res) => {
-    res.send('Welcome to the Pokemon App!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the Pokemon App!");
+});
 
 //create a GET route /pokemon that will display the pokemon data
-app.get('/pokemon', (req,res) => {
-    res.send(pokemon)
-})
+app.get("/pokemon", (req, res) => {
+  res.render("Index");
+});
---
+++ views/Index.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+const style = {
+  color: "#ffffff",
+  backgroundColor: "#000000",
+};
+
+function Index() {
+  return (
+  <div style={style}>Index Page</div>
+  );
+}
+
+module.exports = Index;
